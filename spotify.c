f9df4bac612faaa2537ca46eac84a363cacde763
MellanoMatteo <71833116+MellanoMatteo@users.noreply.github.com>
2020-11-02 19:57:15 +0100

    implmentazione funzione random
    aggiunta della funzione che ordina in modo casuale le canzoni e le da in output sul terminale

--- spotify.c
+++ spotify.c
@@ -8,6 +8,8 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 #define MAX 100        // definisco la lunghezza dei buffer
 #define NSONG 10       //definisco il nuemro delle canzoni
@@ -16,24 +18,31 @@ typedef struct s_canzone{
     int numero;
     char titolo[MAX];
     char artista[MAX];
+    bool riprodotta;
 }canzone;
 
-void letturaFile(canzone *playlist);
+int  letturaFile(canzone *playlist);
+
+void songRand(canzone *playlist, int numSong);
 
 int main ()
 {   
     canzone playlist[NSONG];                // array di tutte che contiene tutte le struct alle canzoni
+    int numCanzoni;
     
-    letturaFile(playlist);
+    numCanzoni = letturaFile(playlist);
 
+    songRand(playlist, numCanzoni);
+
+    /* DEBUG
     printf("\n\n"); 
-    for (int  i = 0; i < NSONG; i++)        //stampa il contenuto delle struct per vedere cosa mi mette
-        printf("%d-%s-%s\n", playlist[i].numero, playlist[i].artista, playlist[i].titolo);
+    for (int  i = 0; i < NSONG; i++)        
+        printf("%d-%s-%s\n", playlist[i].numero, playlist[i].artista, playlist[i].titolo); */
 
     return 0;
 }
 
-void letturaFile(canzone *playlist)
+int letturaFile(canzone *playlist)
 {
     char buf[MAX];                          // variabile in cui salvo l'intera riga per poi analizzarla
     char* token;                            //variabile che mi serve per salvare i valori restituiti da strtok
@@ -61,11 +70,53 @@ void letturaFile(canzone *playlist)
             playlist[nCanzone].artista[strlen(playlist[nCanzone].artista)-1] = '\0';    // elimino l'ultimo carattere, ovvero quello di messa a capo, dall'interno della stringa dell'artista
             printf("artista: %s \t\t\n",playlist[nCanzone].artista);
 
-            //FUNZIONAMENTO STRTOK: passanfogli null la funzione strtok capisce che lei deve andare avanti sulla stessa riga precedentemente passata
+            //FUNZIONAMENTO STRTOK: passandogli null la funzione strtok capisce che lei deve andare avanti sulla stessa riga precedentemente passata
         }
-
         fclose(fp); 
+        return nCanzone; // ritorno il numero di canzoni totale 
     }
     else
         printf("errore durante l'apertura del file \n\n\n");
+}
+
+void songRand(canzone *playlist, int numSong)
+{
+    srand(time(NULL));                                  //fuznione che mi permette di utilizzare la funzione rand per il calcolo di numeri casuali
+    int volteRiprodotte = 3;                            //numero di volte che la playlist viene riprodotta nella sua interezza 
+    int random = 0;
+    /*
+    for(per n volte)
+    {
+        for(per tutte le canzoni )
+        {
+            do
+            {
+                tira a sorte una canzone 
+            }while(se la canzone è già stata riprodotta)
+            riproducila 
+        }
+    }
+    */
+    printf("\n\nfunzione stampa:\n\n\n");
+
+    for (int i = 0; i < numSong; i++)                   //porto tutte le canzoni a false per contrassegnare le canzoni già riprodotte
+            playlist[i].riprodotta = false;
+
+    for (int j = 0; j < volteRiprodotte; j++)           // per un numero a piacere di volte che si vuole riprodurre la playlist
+    {
+        printf("\n\nriproduzione n %d:\n\n\n", j);
+        for (int i = 0; i < numSong; i++)               //per tutte le canzoni della playlist
+        {
+            do{
+                random = rand() % numSong;              //tiro a caso un numero tra 0 e rand
+
+            }while(playlist[random].riprodotta);              //controllo se la canzone sorteggiata è già stata riprodotta
+
+            playlist[random].riprodotta = true;              //porto riprodotta a true per contrassegnare la sua avvenuta riproduzione 
+            
+            printf("titolo: %s, artista: %s\n", playlist[random].titolo, playlist[random].artista); // printo gli estremi della canzone 
+        }
+        for (int i = 0; i < numSong; i++)               //riporto tutte le canzoni ad un valore false per poter riutilizzare la variabile booleana
+            playlist[i].riprodotta = false;
+    }
 }
