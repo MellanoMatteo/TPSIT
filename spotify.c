bdd398de646aa275cea143c33600aaea9db39426
Matteo Mellano <matteo.mellano.s@itiscuneo.eu>
2020-11-05 18:51:55 +0100

    modifica malloc
    array delle canzoni ora allocato dinamicamente e gli array gestiti con la logica dei puntatori

--- spotify.c
+++ spotify.c
@@ -1,10 +1,3 @@
-//obiettivo:riprodurre la funzione di spotify che fa partire le canzoni in modo casuale
-
-/*
-1. lettura csv (leggere tutto il valueStringaenuto del file e riconoscere la virgola: 0,titolo,autore)
-    1.1 caricamento(caricare i file letti in memoria) ovvero in array di struttura
-2. random ordering (sorteggio delle canzoni in modo tale che non si ripetino )
-*/
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -21,62 +14,57 @@ typedef struct s_canzone{
     bool riprodotta;
 }canzone;
 
-int  letturaFile(canzone *playlist);
-
 void songRand(canzone *playlist, int numSong);
 
 int main ()
 {   
-    canzone playlist[NSONG];                // array di tutte che contiene tutte le struct alle canzoni
+    canzone* playlist;                      // array di tutte che contiene tutte le struct alle canzoni
     int numCanzoni;
-    
-    numCanzoni = letturaFile(playlist);
 
-    songRand(playlist, numCanzoni);
-
-    /* DEBUG
-    printf("\n\n"); 
-    for (int  i = 0; i < NSONG; i++)        
-        printf("%d-%s-%s\n", playlist[i].numero, playlist[i].artista, playlist[i].titolo); */
-
-    return 0;
-}
-
-int letturaFile(canzone *playlist)
-{
     char buf[MAX];                          // variabile in cui salvo l'intera riga per poi analizzarla
     char* token;                            //variabile che mi serve per salvare i valori restituiti da strtok
-    const char *ch = ",";                   // mi serve per effettuare la separazione della stringa 
+    const char* ch = ",";                   // mi serve per effettuare la separazione della stringa 
     int nCanzone = 0;                       // in fase di inizializzazione, mi serve per controllare di scorrere tutte le canzoni 
+    int contMalloc = 0;
 
-    FILE *fp=fopen("playlist.csv","r");
+    FILE* fp = fopen("playlist.csv","r");       
+    FILE* fAppoggio = fopen("playlist.csv","r");                        //creo un doppione del puntatore al file per poter scorrere il file due volte
 
-    if(fp != NULL)
+    if(fp)
     {
-        while(fgets(buf, MAX, fp))                    //finchè ci sono righe da leggere (ovvero finchè non sono arrivato alla fine del file)
+        while (fgets(buf, MAX, fp))                                     // conto le canzoni del file per sapere a quanto inizializzare l'array di canzoni con la malloc
+            contMalloc++;
+
+        playlist = (canzone*) malloc(contMalloc * sizeof(canzone));     //utilizzo la malloc per creare un array dinamico
+
+        while(fgets(buf, MAX, fAppoggio))                               //finchè ci sono righe da leggere (ovvero finchè non sono arrivato alla fine del file)
         {
                         //prendo una riga del file 
 
             token = strtok(buf, ch);        //strtok ritorna la prima occorrenza del secondo parametro all'interno del primo
 
-            playlist[atoi(token)].numero = atoi(token);                                 // lo leggo prendendolo come decimale (atoi converte da stringa a decimale)
-            printf("numero: %d \t\t",playlist[atoi(token)].numero);                     //debug
-            nCanzone = atoi(token);                                                     //assegno alla variabile nCanzone il valore del numero corrente della canzone
+            (playlist+atoi(token))->numero = atoi(token);                                   // lo leggo prendendolo come decimale (atoi converte da stringa a decimale)
+            printf("numero: %d \t\t", (playlist+atoi(token))->numero);                      //debug
+            nCanzone = atoi(token);                                                         //assegno alla variabile nCanzone il valore del numero corrente della canzone
             
-            strcpy(playlist[nCanzone].titolo, strtok(NULL, ch));                        //leggo la 2a stringa del file, che corrisponde al titolo, e lo salvo nella parte apposita all'interno della struct
-            printf("titolo: %s \t\t",playlist[nCanzone].titolo);
+            strcpy((playlist+nCanzone)->titolo, strtok(NULL, ch));                          //leggo la 2a stringa del file, che corrisponde al titolo, e lo salvo nella parte apposita all'interno della struct
+            printf("titolo: %s \t\t", (playlist+nCanzone)->titolo);
             
-            strcpy(playlist[nCanzone].artista, strtok(NULL, ch));                       //copio il valore restituito dalla lettura del file con strtok all'interno della struct
-            playlist[nCanzone].artista[strlen(playlist[nCanzone].artista)-1] = '\0';    // elimino l'ultimo carattere, ovvero quello di messa a capo, dall'interno della stringa dell'artista
-            printf("artista: %s \t\t\n",playlist[nCanzone].artista);
+            strcpy((playlist+nCanzone)->artista, strtok(NULL, ch));                         //copio il valore restituito dalla lettura del file con strtok all'interno della struct
+            (playlist+nCanzone)->artista[strlen((playlist+nCanzone)->artista)-1] = '\0';    // elimino l'ultimo carattere, ovvero quello di messa a capo, dall'interno della stringa dell'artista
+            printf("artista: %s\n", (playlist+nCanzone)->artista);
 
             //FUNZIONAMENTO STRTOK: passandogli null la funzione strtok capisce che lei deve andare avanti sulla stessa riga precedentemente passata
         }
-        fclose(fp); 
-        return nCanzone; // ritorno il numero di canzoni totale 
+        fclose(fp);  
     }
     else
         printf("errore durante l'apertura del file \n\n\n");
+
+    songRand(playlist, nCanzone);
+
+    free(playlist);
+    return 0;
 }
 
 void songRand(canzone *playlist, int numSong)
@@ -100,7 +88,7 @@ void songRand(canzone *playlist, int numSong)
     printf("\n\nfunzione stampa:\n\n\n");
 
     for (int i = 0; i < numSong; i++)                   //porto tutte le canzoni a false per contrassegnare le canzoni già riprodotte
-            playlist[i].riprodotta = false;
+            (playlist + i)->riprodotta = false;
 
     for (int j = 0; j < volteRiprodotte; j++)           // per un numero a piacere di volte che si vuole riprodurre la playlist
     {
@@ -110,13 +98,13 @@ void songRand(canzone *playlist, int numSong)
             do{
                 random = rand() % numSong;              //tiro a caso un numero tra 0 e rand
 
-            }while(playlist[random].riprodotta);              //controllo se la canzone sorteggiata è già stata riprodotta
+            }while((playlist + random)->riprodotta);              //controllo se la canzone sorteggiata è già stata riprodotta
 
-            playlist[random].riprodotta = true;              //porto riprodotta a true per contrassegnare la sua avvenuta riproduzione 
+            (playlist+random)->riprodotta = true;              //porto riprodotta a true per contrassegnare la sua avvenuta riproduzione 
             
-            printf("titolo: %s, artista: %s\n", playlist[random].titolo, playlist[random].artista); // printo gli estremi della canzone 
+            printf("titolo: %s, artista: %s\n", (playlist + random)->titolo, (playlist + random)->artista); // printo gli estremi della canzone 
         }
         for (int i = 0; i < numSong; i++)               //riporto tutte le canzoni ad un valore false per poter riutilizzare la variabile booleana
-            playlist[i].riprodotta = false;
+            (playlist + i)->riprodotta = false;
     }
-}
+}}
